d77e7de0c1ea666f149e954ebc71801dd61d8e84
ArielFJ <arielfj@magnanym.com>
2020-03-12 16:04:10 -0400

    Agregados componentes de react de las opciones del menu de navegacion

---
+++ src/components/Basica.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+export class Suma extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="display-4" >Suma</h1>
+            </div>
+        )
+    }
+}
+
+export class Resta extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="display-4" >Resta</h1>
+            </div>
+        )
+    }
+}
+
+export class Multiplicacion extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="display-4" >Multiplicacion</h1>
+            </div>
+        )
+    }
+}
+
+export class Division extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="display-4" >Division</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/Prealgebra.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+export class Potencia extends Component {
+    render() {
+        return (
+            <div>
+                Potencia
+            </div>
+        )
+    }
+}
+
+export class RaizCuadrada extends Component {
+    render() {
+        return (
+            <div>
+                Raiz cuadrada
+            </div>
+        )
+    }
+}
+
+export class RaizCubica extends Component {
+    render() {
+        return (
+            <div>
+                Raiz cubica
+            </div>
+        )
+    }
+}
+
+export class Factorial extends Component {
+    render() {
+        return (
+            <div>
+                factorial
+            </div>
+        )
+    }
+}
